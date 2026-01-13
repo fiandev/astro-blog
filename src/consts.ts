e1a6b01704307ffbd2a579bfd911379e2ad7ae0f
fiandev <alfiansa@proton.me>
2026-01-13 10:04:56 +0700

    feat: setup layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "fian's blog";
+export const SITE_DESCRIPTION =
+  "Blog pribadi dari seoarang programmer bernama fian, sebagai tempat untuk berbagi pengetahuan dan pengalaman";
