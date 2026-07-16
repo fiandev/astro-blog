0aa2d33977ff5075c688b20e8d507c05d0eb142b
fiandev <alfiansa@proton.me>
2026-07-16 11:13:57 +0700

    fix: clean up not found image asset url

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,30 +1,3 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
-      read_time: z.number().optional(),
-      categories: z.array(z.string()).optional(),
-      slug: z.string(),
-      views: z.number().default(0),
-      author: z
-        .object({
-          name: z.string(),
-          bio: z.string().optional(),
-          gravatar: z.string().optional(),
-        })
-        .optional(),
-    }),
-});
-
-export const collections = { blog };
+export const collections = {};
